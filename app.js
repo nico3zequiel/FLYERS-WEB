7d68e7a4c780a5d7eb2d2e83143f4e1fd9f4e6c3
unknown <jofrenicolas60@gmail.com>
2022-03-11 01:21:49 -0300

    Imagenes agregadas a seccion HOME

---
+++ app.js
@@ -0,0 +1,11 @@
+$(".card").on("click", function () {
+  let me = this;
+  this.classList.toggle("large");
+  let cards = $(".card");
+  cards.each(function (index, element) {
+    let card = element;
+    if (card !== me) {
+      card.classList.remove("large");
+    }
+  });
+});
